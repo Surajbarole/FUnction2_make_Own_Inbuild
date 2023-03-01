9409fd50be662e94237cc33fed75db6a5d179033
Suraj Barole <103420352+Surajbarole@users.noreply.github.com>
2023-03-01 05:19:40 +0000

    Initial commit

---
+++ sol1.js
@@ -0,0 +1,9 @@
+// Problem 1. Write a custom function that has the same behavior of inbuilt Array Join Function -
+
+let arr=["fire","hero","suraj"];
+
+let bag="";
+for(let i=0;i<=arr.length-1;i++){
+  bag+=arr[i]+" ";
+}
+console.log(bag)
---
+++ sol2.js
@@ -0,0 +1,10 @@
+// Write a custom function that has the same behavior of inbuilt Array Last Index Of Function
+function lastIn(arr){
+let count=0;
+for(let i=0;i<=arr.length-1;i++){
+  count++
+
+console.log(["dedo"].length)
+}
+}
+lastIn("Tiger","suraj","dedo")
